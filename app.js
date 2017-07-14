e964cb9022268c46873d3175bc5f219bc4e16a45
pennygibson <cenneboulet@yahoo.ca>
2017-07-14 02:40:12 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $(".fudgeLocations").show("slow");             
+    })
+    $("#button2").click(function(){
+        $(".flavourList").show("slow");
+    })
+})
+
+
